2695e00eb197f2b0fe290367b946ac65239ada6c
sevketasurgan <43352546+sevketasurgan@users.noreply.github.com>
2022-11-13 14:10:40 +0300

    Update KdvTutar.java

--- KdvTutar.java
+++ KdvTutar.java
@@ -9,13 +9,21 @@ public class KdvTutar {
         Scanner input = new Scanner(System.in);
         System.out.print("Ücret tutarını giriniz : ");
         tutar = input.nextDouble();
-        kdvliFiyat = tutar * 1.18;
+        boolean res = tutar > 0 && tutar < 1000 ? true : false;
+        if(res){
+            kdvliFiyat = tutar * 1.18;
         kdvTutar = Math.abs(kdvliFiyat-tutar);
-
+         
         System.out.println("Kdv'siz fiyat : " + tutar);
         System.out.println("Kdv'li fiyat : " + kdvliFiyat);
         System.out.println("Kdv eklenen fiyat : " + kdvTutar);
         System.out.println("Kdv Oranı  : " + kdvOran);
+            
+        }else{
+            System.out.println("Lütfen 0 ile 1000 tl arasında tutar giriniz ! ");
+        }
+        
+        
 
 
     }
