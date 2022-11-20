7919b1ce3a72de8dfadc8e572aea19cace520e89
sevketasurgan <43352546+sevketasurgan@users.noreply.github.com>
2022-11-20 23:53:04 +0300

    Add files via upload

---
+++ Horoscope.java
@@ -0,0 +1,78 @@
+package Giris;
+
+import java.util.Scanner;
+
+public class Horoscope {
+    public static void main(String[] args) {
+        /*
+        Koç Burcu : 21 Mart - 20 Nisan
+
+        Boğa Burcu : 21 Nisan - 21 Mayıs
+
+        İkizler Burcu : 22 Mayıs - 22 Haziran
+
+        Yengeç Burcu : 23 Haziran - 22 Temmuz
+
+        Aslan Burcu : 23 Temmuz - 22 Ağustos
+
+        Başak Burcu : 23 Ağustos - 22 Eylül
+
+        Terazi Burcu : 23 Eylül - 22 Ekim
+
+        Akrep Burcu : 23 Ekim - 21 Kasım
+
+        Yay Burcu : 22 Kasım - 21 Aralık
+
+        Oğlak Burcu : 22 Aralık - 21 Ocak
+
+        Kova Burcu : 22 Ocak - 19 Şubat
+
+        Balık Burcu : 20 Şubat - 20 Mart
+         */
+
+
+        int day, month;
+        boolean isErr = false;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Lütfen doğduğunuz ayı giriniz :");
+        month = input.nextInt();
+
+
+        System.out.print("Doğduğunuz gün : ");
+        day = input.nextInt();
+
+        if ((month == 12 && day >= 22 && day <= 31) || (month == 1 && day >= 1 && day <= 19))
+            System.out.println("Oğlak");
+        else if ((month == 1 && day >= 20 && day <= 31) || (month == 2 && day >= 1 && day <= 17))
+            System.out.println("Kova");
+        else if ((month == 2 && day >= 18 && day <= 29) || (month == 3 && day >= 1 && day <= 19))
+            System.out.println("Balık");
+        else if ((month == 3 && day >= 20 && day <= 31) || (month == 4 && day >= 1 && day <= 19))
+            System.out.println("Koç");
+        else if ((month == 4 && day >= 20 && day <= 30) || (month == 5 && day >= 1 && day <= 20))
+            System.out.println("Boğa");
+        else if ((month == 5 && day >= 21 && day <= 31) || (month == 6 && day >= 1 && day <= 20))
+            System.out.println("İkizler");
+        else if ((month == 6 && day >= 21 && day <= 30) || (month == 7 && day >= 1 && day <= 22))
+            System.out.println("yengeç");
+        else if ((month == 7 && day >= 23 && day <= 31) || (month == 8 && day >= 1 && day <= 22))
+            System.out.println("Aslan");
+        else if ((month == 8 && day >= 23 && day <= 31) || (month == 9 && day >= 1 && day <= 22))
+            System.out.println("başak");
+        else if ((month == 9 && day >= 23 && day <= 30) || (month == 10 && day >= 1 && day <= 22))
+            System.out.println("Terazi");
+        else if ((month == 10 && day >= 23 && day <= 31) || (month == 11 && day >= 1 && day <= 21))
+            System.out.println("Akrep");
+        else if ((month == 11 && day >= 22 && day <= 30) || (month == 12 && day >= 1 && day <= 21))
+            System.out.println("Yay");
+        else
+            System.out.println("Hatalı Giriş");
+
+    }
+
+
+
+
+}
